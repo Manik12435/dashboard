8a3f2775f395950c42684a658786fede8f36449a
Manik12435 <manikanhkhk@gmail.com>
2022-11-15 17:56:33 +0800

    LoginChanges

--- cfx-dashboard/src/app/app.service.ts
+++ cfx-dashboard/src/app/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +9,16 @@ import { environment } from 'src/environments/environment';
 export class AppService {
   auth: any;
 
-  constructor() { }//private http: HttpClient
+  constructor(private http: HttpClient) { }//private http: HttpClient
 
 
-  // login(request: any){
-  //   let URL = environment.api.login;
-  //   return this.http.post(URL, request)//.pipe(map((response: any) => <any>response))
-  // }
+  login(request: any){
+    let URL = environment.api.login;
+    return this.http.post(URL, request)//.pipe(map((response: any) => <any>response))
+  }
 
-  // sample(){
-  //   let URL = environment.api.sample;
-  //   return this.http.get(URL, {responseType: 'json', headers: {'Authorization': this.auth}})
-  // }
+  sample(){
+    let URL = environment.api.sample;
+    return this.http.get(URL, {responseType: 'json', headers: {'Authorization': this.auth}})
+  }
 }
